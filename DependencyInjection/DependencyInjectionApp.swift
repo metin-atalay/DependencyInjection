b86116192b4dcf967e0ae240ed031812ac1db84b
metin-atalay <tr.metin.atalay@gmail.com>
2022-05-01 10:31:41 +0300

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Metin Atalay on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
